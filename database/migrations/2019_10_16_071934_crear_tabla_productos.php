def7756b1fa4b6cd602219287d5a33f4fbd54a32
AnderGonzalezA <agonzalezam18dw@ikzubirimanteo.com>
2019-10-16 13:39:25 +0200

    conexion con la base de datos y migraciones hechas

---
+++ database/migrations/2019_10_16_071934_crear_tabla_productos.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CrearTablaProductos extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nombre');
+            $table->string('descripcion');
+            $table->string('imagen')->nullable();
+            $table->integer('stock');
+            $table->string('enlaces')->nullable();
+            $table->integer('tienda_id')->unsigned();
+            $table->foreign('tienda_id')->references('id')->on('tiendas');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+}
